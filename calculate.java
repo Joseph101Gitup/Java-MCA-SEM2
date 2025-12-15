b29d957e0750bd4a4fdc7fc0b6d0ff1cecd67736
Joseph101Gitup <josephpaul200416@gamil.com>
2025-12-15 14:49:26 +0530

    Initial commit

--- calculate.java
+++ calculate.java
@@ -1,6 +1,5 @@
-import java.io.*;
 
-class Calculate {
+class calculate {
     public static void main(String args[]) {
         if(args.length < 3) {
             System.out.println("Usage: java Calculate <n1> <n2> <operation>");
